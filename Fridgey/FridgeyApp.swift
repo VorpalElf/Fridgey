aaa6537cd38c5c3cdb0b3a717f43539e3635bd4e
VorpalElf <jeremy20131085@gmail.com>
2026-06-02 13:57:49 +0100

    Initial Commit

---
+++ Fridgey/FridgeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeyApp.swift
+//  Fridgey
+//
+//  Created by Jeremy Lo on 02/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct FridgeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
